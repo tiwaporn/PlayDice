b13f3d0a84589ca5ceb3a1c3471df76e95320383
muk <muk@abc.com>
2019-03-18 15:51:34 +0700

    Initial Commit

---
+++ PlayDice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayDice
+//
+//  Created by iMacStudent25 on 18/3/2562 BE.
+//  Copyright © 2562 Tiwaporn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PlayDiceTests/PlayDiceTests.swift
@@ -0,0 +1,34 @@
+//
+//  PlayDiceTests.swift
+//  PlayDiceTests
+//
+//  Created by iMacStudent25 on 18/3/2562 BE.
+//  Copyright © 2562 Tiwaporn. All rights reserved.
+//
+
+import XCTest
+@testable import PlayDice
+
+class PlayDiceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
