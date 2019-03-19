6d9735348bf1c2f423bcae4463ecacfddc97de6a
muk <muk@abc.com>
2019-03-19 11:50:42 +0700

    play dice

--- PlayDice/ViewController.swift
+++ PlayDice/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstDiceImageView: UIImageView!
+    
+    @IBOutlet weak var secondDiceimageview: UIImageView!
+    
+    @IBOutlet weak var ThirdDticeimageview: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    }  //Main Method
+    
+    @IBAction func RamdomDiceButton(_ sender: UIButton) {
+        
+     print ("You Click Ramdom")
+        
+    }   //ramdomDice
+    
+    
 
 
-}
+}  //Main Class
 
